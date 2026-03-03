3b3872a56c3dd16709f044c3faf240f3ee8acb7b
treborfal <77328069+treborfal@users.noreply.github.com>
2026-03-03 12:26:34 +0000

    Use requestAnimationFrame for header scroll handling

--- js/abovethefold.js
+++ js/abovethefold.js
@@ -1,39 +1,49 @@
 // Hide header on scroll down
-var didScroll;
 var lastScrollTop = 0;
 var delta = 5;
-var navbarHeight = $('header').outerHeight();
+var header = document.querySelector('header');
+var navbarHeight = header ? header.offsetHeight : 0;
+var isTicking = false;
 
-$(window).scroll(function(event){
-    didScroll = true;
-});
-
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
+// Throttle scroll work to animation frames so layout/class updates do not run on every scroll event.
+window.addEventListener('scroll', function () {
+    if (isTicking) {
+        return;
     }
-}, 250);
+
+    isTicking = true;
+    window.requestAnimationFrame(function () {
+        hasScrolled();
+        isTicking = false;
+    });
+}, { passive: true });
 
 function hasScrolled() {
-    var st = $(this).scrollTop();
-    
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
+    if (!header) {
         return;
-    
+    }
+
+    var st = window.scrollY || window.pageYOffset || 0;
+
+    // Make sure they scroll more than delta
+    if (Math.abs(lastScrollTop - st) <= delta) {
+        return;
+    }
+
     // If they scrolled down and are past the navbar, add class .nav-up.
     // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
+    if (st > lastScrollTop && st > navbarHeight) {
         // Scroll Down
-        $('header').removeClass('nav-down').addClass('nav-up');
+        header.classList.remove('nav-down');
+        header.classList.add('nav-up');
     } else {
         // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('header').removeClass('nav-up').addClass('nav-down');
+        if (st + window.innerHeight < document.documentElement.scrollHeight) {
+            header.classList.remove('nav-up');
+            header.classList.add('nav-down');
         }
     }
-    
+
     lastScrollTop = st;
 }
 
