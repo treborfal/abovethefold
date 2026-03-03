55ae51a14cb40580ebad6b82ffe68e5f91858e6c
treborfal <77328069+treborfal@users.noreply.github.com>
2026-03-03 07:36:08 +0000

    Implement typo/bug/docs fixes and add link rendering tests

---
+++ tests/links-renderer.test.js
@@ -0,0 +1,101 @@
+const test = require('node:test');
+const assert = require('node:assert/strict');
+
+const linksRenderer = require('../js/links-renderer.js');
+
+class FakeElement {
+  constructor(tagName) {
+    this.tagName = tagName;
+    this.children = [];
+    this.className = '';
+    this.href = '';
+    this.target = '';
+    this.title = '';
+    this.rel = '';
+    this.textContent = '';
+  }
+
+  appendChild(child) {
+    this.children.push(child);
+    return child;
+  }
+
+  get childElementCount() {
+    return this.children.length;
+  }
+}
+
+class FakeDocument {
+  constructor(container) {
+    this.container = container;
+  }
+
+  createElement(tagName) {
+    return new FakeElement(tagName);
+  }
+
+  getElementById(id) {
+    return id === 'links-list' ? this.container : null;
+  }
+}
+
+test('renderSecondaryLinks appends all links with target=_blank', () => {
+  const container = new FakeElement('div');
+  const documentRef = new FakeDocument(container);
+
+  const count = linksRenderer.renderSecondaryLinks(
+    container,
+    [
+      {
+        linkTitle: 'There Is No Fold',
+        linkURL: 'https://www.lukew.com/ff/entry.asp?1946',
+        linkSource: 'lukew.com'
+      },
+      {
+        linkTitle: 'As the Page Scrolls',
+        linkURL: 'https://articles.uie.com/page_scrolling/',
+        linkSource: 'articles.uie.com'
+      }
+    ],
+    documentRef
+  );
+
+  assert.equal(count, 2);
+  assert.equal(container.childElementCount, 2);
+
+  for (const linkItem of container.children) {
+    assert.equal(linkItem.className, 'link');
+    const titleParagraph = linkItem.children[0];
+    const anchor = titleParagraph.children[0];
+    assert.equal(anchor.target, '_blank');
+    assert.equal(anchor.rel, 'noopener noreferrer');
+  }
+});
+
+test('loadSecondaryLinks consumes fetch response and renders links', async () => {
+  const container = new FakeElement('div');
+  const documentRef = new FakeDocument(container);
+
+  const fetchImpl = async () => ({
+    json: async () => ({
+      secondaryLinks: [
+        {
+          linkTitle: 'Scrolling and Attention',
+          linkURL: 'https://www.nngroup.com/articles/scrolling-and-attention/',
+          linkSource: 'nngroup.com'
+        }
+      ]
+    })
+  });
+
+  const renderedCount = await linksRenderer.loadSecondaryLinks({
+    fetchImpl,
+    documentRef,
+    container
+  });
+
+  assert.equal(renderedCount, 1);
+  assert.equal(container.childElementCount, 1);
+  const sourceParagraph = container.children[0].children[1];
+  assert.equal(sourceParagraph.textContent, 'nngroup.com');
+});
